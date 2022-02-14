5a60902289e0b602c24eea16529a69d898ac0404
Michael Akinnike <michaelakinnike@gmail.com>
2022-02-14 14:07:20 +0000

    welcome page 2

---
+++ lib/screens/welcome_page_2.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:befit_app/template/template.dart';
+
+class WelcomePage2 extends StatelessWidget {
+  const WelcomePage2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Template(
+      image: AssetImage('images/Homescreen1.png'),
+      title: '',
+      text1: '',
+      text2: '',
+      text3: '',
+      iconImage: AssetImage(''),
+      navigateTo: '',
+      buttonAction: '',
+    );
+  }
+}
